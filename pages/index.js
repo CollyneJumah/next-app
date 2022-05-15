120a7558e87387e63737cf06ec20cb9f4283d1a3
CollyneJumah <collynejumah2010@gmail.com>
2022-05-15 21:43:11 +0300

    FIX: React unescaped entities

--- pages/index.js
+++ pages/index.js
@@ -22,15 +22,15 @@ export default function Home({allPostData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, am Collins.orem Ipsum has been the industry's standard dum </p>
+        <p>Hello, am Collins.orem Ipsum has been the industry&apos;s standard dum </p>
         <p>
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        Lorem Ipsum has been the industry&lsquo;s standard dummy text ever since the 1500s, 
         when an unknown printer took a galley of type and scrambled it to make a type 
         specimen book. It has survived not only five centuries,but also the leap into 
         electronic typesetting, remaining essentially unchanged.
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you&rsquo;ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
